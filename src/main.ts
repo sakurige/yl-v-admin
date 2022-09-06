177b547cb190dcb676ee8ac195a15cc9680c2ff4
Sakurige <2017782140@qq.com>
2022-09-06 19:57:01 +0800

    fix(custom): 添加环境变量

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,5 @@ import pinia from "./stores";
 // 样式重置
 import "normalize.css";
 import "@/assets/style/index.less";
-
+console.log(import.meta.env.VITE_AXIOS_BASE_URL);
 createApp(App).use(router).use(pinia).mount("#app");
